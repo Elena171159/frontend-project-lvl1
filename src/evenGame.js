d8fb03c5736ca926137f8cfbcdc4da75b112ef2a
Elena Diachkova <zabava1711@bk.ru>
2022-07-08 15:20:20 +0300

    change evenGame

--- src/evenGame.js
+++ src/evenGame.js
@@ -1,12 +1,6 @@
-import readlineSync from 'readline-sync';
-
-let name;
-export const greeting = () => {
-  console.log('Welcome to the Brain Games!');
-  // eslint-disable-next-line no-unused-expressions
-  name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-};
+import {
+  commonAction, getRandom,
+} from './greeting.js';
 
 function even(num) {
   if (num % 2 === 0) {
@@ -14,22 +8,13 @@ function even(num) {
   }
   return 'no';
 }
-export function evenGame() {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let result = 0;
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.ceil(Math.random() * 10);
-    console.log(`Question: ${number}`);
-    const x = even(number);
-    const answer = readlineSync.question('You answer: ');
-    if (x === answer) {
-      result += 1;
-      console.log('Correct!');
-      continue;
-    } else console.log(`"${answer}" is wrong answer ;(. Correct answer was "${x}" \nLet's try again, ${name}`);
-    break;
-  }
-  if (result === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
+const rule = ('Answer "yes" if the number is even, otherwise answer "no".');
+const makeEvenGame = () => {
+  const gameQuestion = getRandom();
+  const gameAnswer = even(gameQuestion);
+  return [gameQuestion, gameAnswer];
+};
+function startevenGame() {
+  commonAction(rule, makeEvenGame);
 }
+export default startevenGame;
