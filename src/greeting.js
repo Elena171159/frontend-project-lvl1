e5aaee5daa1e2de5633ad376efba0b03d0423c0c
Elena Diachkova <zabava1711@bk.ru>
2022-07-08 15:20:59 +0300

    change greeting

--- src/greeting.js
+++ src/greeting.js
@@ -1,8 +1,27 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
+let name;
+
+export function greeting() {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
+  name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+}
+
+export const getRandom = () => Math.ceil(Math.random() * 10);
+
+export const commonAction = (_rule, questionAnswer) => {
+  console.log(_rule);
+  for (let i = 0; i < 3; i += 1) {
+    const [gameQuestion, gameAnswer] = questionAnswer();
+    console.log(`Question: ${gameQuestion}`);
+    const UserAnswer = readlineSync.question('You answer: ');
+    if (gameAnswer === UserAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${UserAnswer}" is wrong answer ;(. Correct answer was "${gameAnswer}" \nLet's try again, ${name}`);
+      return;
+    }
+  }
+  console.log(`Congratulate, ${name}`);
 };
-export default greeting;
