2f8470cfca1f7fe8db8d0e1ed0090182cf3340c5
Elena Diachkova <zabava1711@bk.ru>
2022-07-26 18:22:34 +0300

    change primeGame

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,21 +1,27 @@
-import launchGames from '../index.js';
+import launchGame from '../index.js';
 import getRandom from '../utils.js';
 
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const checkPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  let i = 2;
+  const limit = Math.sqrt(num);
+  while (i <= limit) {
     if (num % i === 0) {
       return false;
     }
+    i += 1;
   }
   return true;
 };
 const makePrimeGame = () => {
   const gameQuestion = getRandom(1, 100);
-  const gameAnswer = checkPrime(gameQuestion) ? 'yes' : 'no';
+  const gameAnswer = isPrime(gameQuestion) ? 'yes' : 'no';
   return [gameQuestion, gameAnswer];
 };
 const startPrimeGame = () => {
-  launchGames(rule, makePrimeGame);
+  launchGame(rule, makePrimeGame);
 };
 export default startPrimeGame;
