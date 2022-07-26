61edf0504633c61cf3ab2b3cdbb9466f414a66f0
Elena Diachkova <zabava1711@bk.ru>
2022-07-26 18:20:59 +0300

    change calculate

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,32 +1,27 @@
-import launchGames from '../index.js';
+import launchGame from '../index.js';
 import getRandom from '../utils.js';
 
 const rule = ('What is the result of the expression?');
-const checkAnswer = (num1, num2, operator) => {
-  let answer;
+const calculate = (num1, num2, operator) => {
   switch (operator) {
-    case '+': answer = num1 + num2;
-      break;
-    case '-': answer = num1 - num2;
-      break;
-    case '*': answer = num1 * num2;
-      break;
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
     default: throw new Error(`Unknown order state: '${operator}'!`);
   }
-  return String(answer);
 };
 const makeCalkGame = () => {
   const operators = ['+', '-', '*'];
   const number1 = getRandom(1, 100);
   const number2 = getRandom(1, 100);
-  const number = getRandom(0, 2);
+  const number = getRandom(0, operators.length - 1);
   const operator = operators[number];
   const gameQuestion = `${number1} ${operator} ${number2}`;
-  const gameAnswer = checkAnswer(number1, number2, operator);
+  const gameAnswer = String(calculate(number1, number2, operator));
 
   return [gameQuestion, gameAnswer];
 };
 function startCalcGame() {
-  launchGames(rule, makeCalkGame);
+  launchGame(rule, makeCalkGame);
 }
 export default startCalcGame;
